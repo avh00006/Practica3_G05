d0e295d334f5f8e38aa3b2a6b1668d3643d19d53
a-venteo <avh00006@estudiante.ujaen.es>
2016-10-06 12:21:51 +0200

    sesion 2

---
+++ app/src/main/java/es/ujaen/git/Practica1/AuthFragment.java
@@ -0,0 +1,87 @@
+package es.ujaen.git.Practica1;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ * Use the {@link AuthFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AuthFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mUser="";
+    private String mPass="";
+
+
+
+
+    public AuthFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param user Parameter 1.
+     * @param pass Parameter 2.
+     * @return A new instance of fragment AuthFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AuthFragment newInstance(String user, String pass) {
+        AuthFragment fragment = new AuthFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, user);
+        args.putString(ARG_PARAM2, pass);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mUser = getArguments().getString(ARG_PARAM1);
+            mPass = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View fragmento = inflater.inflate(R.layout.fragment_auth, container, false);
+
+        EditText user = (EditText)fragmento.findViewById(R.id.user2);
+        EditText pass = (EditText)fragmento.findViewById(R.id.pass);
+
+        user.setText(mUser);
+        pass.setText(mPass);
+
+        return fragmento;
+
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/es/ujaen/git/Practica1/MainActivity.java
+++ app/src/main/java/es/ujaen/git/Practica1/MainActivity.java
@@ -1,7 +1,12 @@
 package es.ujaen.git.Practica1;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        AuthFragment au = AuthFragment.newInstance("pepe","12345");
+        ft.add(R.id.main_frame,au);
+        ft.addToBackStack(null);
+        ft.commit();
+
+        Button boton = (Button)findViewById(R.id.loginbtn);
+
+        boton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String nombre = mEditUser.getText().toString();
+                Autenticacion datos = new Autentication (nombre,null,null,null);
+                Toast.makeText(MainActivity.this,"Pulsado", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onSend(View view){
+        Toast.makeText(this,"Pulsado", Toast.LENGTH_SHORT).show();
     }
 }
