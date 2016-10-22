30d4b7e64199849d25888334d3662af609f9639a
a-venteo <avh00006@estudiante.ujaen.es>
2016-10-22 21:04:28 +0200

    Código final comentado siguiendo la estructura para javadoc

--- app/src/main/java/es/ujaen/git/Practica1/AuthFragment.java
+++ app/src/main/java/es/ujaen/git/Practica1/AuthFragment.java
@@ -1,35 +1,38 @@
 package es.ujaen.git.Practica1;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * to handle interaction events.
- * Use the {@link AuthFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Subclase que extiende de Fragment.
+ * @param ARG_PARAM1 primer parametro
+ * @param ARG_PARAM2 segundo parametro
+ * @param mUser almacenara el nombre de usuario
+ * @param mPuerto almacenara el puerto
  */
+
 public class AuthFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mUser="";
-    private String mPass="";
 
-    private EditText mEditUser=null;
+    private String mUser;
+    private String mPuerto;
+
+    private Registro mRegistro = new Registro("","","",0);
+
+
+    private EditText user = null;
+    private EditText puert = null;
 
 
 
@@ -38,63 +41,77 @@ public class AuthFragment extends Fragment {
     }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
+     * Crea una nueva instancia de este fragmento usando los parametros
+     * proporcionados.
      *
-     * @param user Parameter 1.
-     * @param pass Parameter 2.
-     * @return A new instance of fragment AuthFragment.
+     * @param user Parametro nombre de usuario.
+     * @param puerto Parametro puerto de comunicaciones.
+     * @return Devuelve una nueva instancia del fragmento AuthFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AuthFragment newInstance(String user, String pass) {
+    public static AuthFragment newInstance(String user,String puerto) {
         AuthFragment fragment = new AuthFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, user);
-        args.putString(ARG_PARAM2, pass);
+        args.putString(ARG_PARAM2,puerto);
         fragment.setArguments(args);
         return fragment;
     }
 
+
+    /**
+     *El sistema llama a este método al crear el fragmento
+     * @param savedInstanceState parametro que muestra el estado actual
+     * @param mUser se le asigna el valor creado por la nueva instancia
+     * @param mPuerto se le asigna el valor creado por la nueva instancia
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mUser = getArguments().getString(ARG_PARAM1);
-            mPass = getArguments().getString(ARG_PARAM2);
-        }
+
+        mUser = getArguments().getString(ARG_PARAM1);
+        mPuerto = getArguments().getString(ARG_PARAM2);;
+        mRegistro.setmUser(mUser);
+        mRegistro.setmPuerto(Integer.parseInt(mPuerto));
+
+
+
     }
 
+    /**
+     * El sistema llama este método cuando se necesita dibujar
+     * la interfaz de usuario del fragmento la primera vez
+     * @param inflater infla la vista
+     * @param container es el contenedor padre
+     * @param savedInstanceState recibe los datos almacenados tras un recreado de la actividad
+     * @return devuelve el fragmento creado
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
-        View fragmento = inflater.inflate(R.layout.fragment_auth, container, false);
 
-        mEditUser = (EditText)fragmento.findViewById(R.id.user2);
-        EditText pass = (EditText)fragmento.findViewById(R.id.pass);
+        if(savedInstanceState!=null){
+            Toast.makeText(getActivity(),"Cambio de configuración",Toast.LENGTH_SHORT).show();
 
-        mEditUser.setText(mUser);
-        pass.setText(mPass);
 
-        Button boton = (Button)fragmento.findViewById(R.id.loginbtn);
+        }
 
-        boton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String nombre = mEditUser.getText().toString();
-                Autentication datos = new Autentication (nombre,null,null,0);
-                Toast.makeText(getActivity(), "Nombre "+datos.getUser(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        /**Infla el layout para este fragmento*/
+        View fragmento = inflater.inflate(R.layout.form, container, false);
+
+        /**Identificamos los elementos EditText y los almacenamos*/
+        user = (EditText)fragmento.findViewById(R.id.user2);
+        puert = (EditText)fragmento.findViewById(R.id.puerto);
+        /**Se guardan los datos en los elementos EditText*/
+        mPuerto=""+mRegistro.getmPort();
+        user.setText(mRegistro.getmUser());
+        puert.setText(mPuerto);
 
         return fragmento;
 
+
     }
-
-
-
-
 
 
 }
